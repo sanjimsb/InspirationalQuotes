de49006161b0d5632107787aa881e58b964b1513
Bipin MSB <bpmsb45@gmail.com>
2022-07-24 23:28:07 -0400

    Initial Commit

---
+++ InspiringQuotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InspiringQuotes
+//
+//  Created by Bipin Msb on 2022-07-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
